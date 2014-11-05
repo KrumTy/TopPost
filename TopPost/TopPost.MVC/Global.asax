80051909fe9781e238f76d2904663cdc88deb177
digitalbeas7 <zarichepower@gmail.com>
2014-11-05 16:15:46 +0200

    Initial commit

---
+++ TopPost/TopPost.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TopPost.MVC.MvcApplication" Language="C#" %>
---
+++ TopPost/TopPost.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TopPost.MVC.Startup))]
+namespace TopPost.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
